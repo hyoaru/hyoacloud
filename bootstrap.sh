8f2cff9957d95026cf0663e5b306d3826cb5bedb
hyoaru <the.jadecabrera@gmail.com>
2026-09-02 16:17:35 +0800

    chore: add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+aws organizations enable-aws-service-access --service-principal ram.amazonaws.com
+aws organizations enable-aws-service-access --service-principal sso.amazonaws.com
+aws organizations enable-aws-service-access --service-principal guardduty.amazonaws.com
+aws organizations enable-aws-service-access --service-principal securityhub.amazonaws.com
+aws organizations enable-aws-service-access --service-principal access-analyzer.amazonaws.com
+aws organizations enable-aws-service-access --service-principal config.amazonaws.com
+aws organizations enable-aws-service-access --service-principal account.amazonaws.com
